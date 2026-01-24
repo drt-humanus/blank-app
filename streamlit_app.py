8bf778ff4594fdb2894b66bbd1d9688380e24d77
drt-humanus <trung3ga43@gmail.com>
2026-01-24 20:21:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,10 @@ def load_page():
     </style>
     """, unsafe_allow_html=True)
     
-#st.cache_data(suppress_st_warning=True)
-#@st.cache_data(suppress_st_warning=True)
+@st.cache_data(suppress_st_warning=True)
 def get_pickle_data():
-    #download = open("/workspaces/blank-app/.github/deployed_rfmodel_pca10_nor_smote.sav", "rb")
-    
-    download = BytesIO(requests.get("https://github.com/drt-humanus/blank-app/blob/2319314002c7e992f0f0d39f105d3a6e0c5ffafb/deployed_rfmodel_pca10_nor_smote.sav").content)
-    #download = requests.get("https://www.trim.vn/nnEKYs")
-    #download = open(download.content, "rb")
-    return pickle.load(download)
+    with open("/workspaces/blank-app/.github/deployed_rfmodel_pca10_nor_smote.sav", "rb") as download:
+        return pickle.load(download)
 
 def calculate_risk():
     load_page()
